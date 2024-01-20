38a480dd1b8441db30cfdfac53edb6eaa7dd8b90
nehldotjs <nelsonosuya11@gmail.com>
2024-01-20 20:39:01 +0100

    NEW COMMIT

--- src/screens/OptionTrading.js
+++ src/screens/OptionTrading.js
@@ -1,12 +1,21 @@
-import React from 'react'
-import "../styles/optiontradig.css"
+import React from "react";
+import "../styles/optiontradig.css";
+import Footer from "./subpages/Footer";
+import Nav from "../Components/Nav";
+import SubHeaders from "./subpages/SubHeaders";
 
 function OptionTrading() {
   return (
-    <div className='opt-sectionWrapper'>
+    <div className="opt-sectionWrapper">
+      <Nav />
+      <SubHeaders
+        header={"Option Trading"}
+        description={"Options Unleashed: Trading Your Potential."}
+      />
       <h1>OPTION TRADING hello world</h1>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default OptionTrading
+export default OptionTrading;
