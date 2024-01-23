2293faac49d4c5b8b41c67ed27efed47dcf74f68
nehldotjs <nelsonosuya11@gmail.com>
2024-01-23 19:07:06 +0100

    NEW COMMIT

--- src/screens/Estate.js
+++ src/screens/Estate.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import "../styles/estate.css";
+
+import Nav from "../Components/Nav";
+import SubHeader from "../screens/subpages/SubHeaders";
+import Footer from "../screens/subpages/Footer";
 
 function Estate() {
   return (
-    <div>
-      <h1>REAL ESTATE </h1>
+    <div className="estate-main-wrapper">
+      <Nav />
+      <SubHeader
+        description="Building Wealth, One Property at a Time – Invest in Your Future with Real Estate."
+        header="Real Estate"
+      />
+      <h1>REAL ESTATE here</h1>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default Estate
+export default Estate;
