c6586c39c3786079db07070df7054e45984a5daa
nehldotjs <nelsonosuya11@gmail.com>
2024-01-25 19:28:48 +0100

    NEW COMMIT

--- src/screens/Infrastructure.js
+++ src/screens/Infrastructure.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import "../styles/infrastructure.css";
 
 function Infrastructure() {
   return (
-    <div>
+    <div className="inf-main-wrapper">
       <h1>Infrastructure</h1>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, deserunt
+      incidunt! Sapiente officia natus, sunt at distinctio quasi, aliquam,
+      cupiditate fuga repellat voluptatum exercitationem. Reiciendis porro
+      voluptatibus, neque aspernatur tempore quidem sed officia. Minima ratione
+      autem asperiores perferendis cumque reiciendis cupiditate! Aut nesciunt
+      culpa alias architecto quam, amet error aspernatur.
     </div>
-  )
+  );
 }
 
-export default Infrastructure
+export default Infrastructure;
