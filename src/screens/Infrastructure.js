45cd1841bb2d7db2d24957b7a31e99fecc749a94
nehldotjs <nelsonosuya11@gmail.com>
2024-01-28 04:32:13 +0100

    NEW COMMIT

--- src/screens/Infrastructure.js
+++ src/screens/Infrastructure.js
@@ -1,129 +1,90 @@
 import React from "react";
 import "../styles/infrastructure.css";
+import infImg from "../assets/infrastructure.jpg";
+import mapImg from "../assets/sv.png";
+
+import { Screener, TickerTape } from "react-ts-tradingview-widgets";
+
+import Nav from "../Components/Nav";
+import SubHeaders from "./subpages/SubHeaders";
+import Footer from "./subpages/Footer";
+import GetStartedBtn from "./subpages/GetStartedBtn";
 
 function Infrastructure() {
   return (
     <div className="inf-main-wrapper">
-      <h1>Infrastructure</h1>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, deserunt
-      incidunt! Sapiente officia natus, sunt at distinctio quasi, aliquam,
-      cupiditate fuga repellat voluptatum exercitationem. Reiciendis porro
-      voluptatibus, neque aspernatur tempore quidem sed officia. Minima ratione
-      autem asperiores perferendis cumque reiciendis cupiditate! Aut nesciunt
-      culpa alias architecto quam, amet error aspernatur. Lorem ipsum dolor sit,
-      amet consectetur adipisicing elit. Dolorum nulla, magnam debitis ipsa
-      dolorem blanditiis harum iusto officiis maxime eaque rem, sed sit
-      distinctio nam dolor tenetur voluptatem deserunt omnis neque corporis
-      obcaecati minima. Rem mollitia asperiores recusandae excepturi expedita.
-      Excepturi similique earum omnis optio vero deserunt quibusdam consequatur
-      distinctio nisi praesentium error nam id corporis fugit blanditiis
-      expedita nihil ullam, dignissimos, exercitationem quod! Ullam tenetur
-      obcaecati enim voluptates reprehenderit illum, rem provident? Debitis
-      repellendus numquam optio, qui quam ratione quia. Ex ducimus ullam quae,
-      quam corporis vitae, doloremque sunt quo, optio id obcaecati? Itaque error
-      earum consequatur ad temporibus ea nostrum dolor, quibusdam, expedita
-      reprehenderit incidunt perspiciatis accusamus enim ducimus accusantium
-      necessitatibus obcaecati delectus excepturi optio. Blanditiis ut ducimus
-      quos consequatur sed recusandae exercitationem omnis aliquid nesciunt vero
-      enim explicabo ab quasi, adipisci doloribus error voluptatem quisquam
-      tempore. Consectetur, provident, illum soluta, nobis illo suscipit in
-      quisquam quam cupiditate ratione mollitia atque minima sit. Quas ipsam
-      aliquam vitae, alias debitis nemo recusandae laudantium a qui amet
-      consectetur. Temporibus cumque ut quidem nostrum rerum doloribus facilis
-      modi, ipsa ducimus quisquam voluptatum distinctio eius nihil iure dolore?
-      Saepe delectus explicabo dignissimos inventore reprehenderit. Sapiente
-      ratione inventore expedita minus quasi neque, earum vel placeat eaque
-      exercitationem, deserunt enim aspernatur sunt saepe non, illum ab
-      repellendus maxime porro. Officiis sit quidem dolorum ratione nemo soluta!
-      Itaque, autem. Error nesciunt, earum pariatur perferendis repellendus,
-      nulla dolores maiores recusandae assumenda necessitatibus ipsa velit?
-      Tenetur exercitationem delectus aliquam laboriosam soluta fugiat cumque
-      odit alias, explicabo officia ipsum temporibus earum ipsam accusantium
-      corporis ut nobis mollitia asperiores quibusdam commodi iure consequatur
-      amet placeat. Fugiat commodi necessitatibus doloribus accusamus temporibus
-      corrupti obcaecati eligendi ducimus odit consectetur, consequuntur quae
-      nobis quos cumque maxime similique culpa dolores minima esse eius
-      voluptate harum tempore voluptatibus. Debitis delectus repellendus fugit
-      quo minus. Ea sit delectus fugiat explicabo voluptate quidem molestiae
-      ipsum saepe eos minus harum voluptatem sed vel accusamus vitae quo,
-      eligendi distinctio veniam corporis ipsa. Quos quo neque dicta adipisci
-      aut ratione, fuga deserunt, laboriosam placeat praesentium pariatur
-      impedit ullam autem optio est, asperiores fugit dolore iste beatae ducimus
-      sunt deleniti nesciunt magni suscipit. Ad voluptatibus esse cum earum?
-      Perspiciatis, rerum sit ipsam sapiente eum nulla assumenda necessitatibus
-      soluta! Dolore nobis temporibus velit tempora magni, quis nulla sapiente
-      cumque fuga, libero atque repellat. Suscipit, exercitationem.
-      Exercitationem ratione dolor, excepturi praesentium asperiores, et aut,
-      cumque corporis odit eos amet accusantium in velit porro voluptatibus at
-      impedit voluptas dignissimos tempore omnis vero accusamus minus maxime
-      quo? Sunt inventore dicta quae, illum reprehenderit omnis ex, maxime
-      cumque aliquam dolorum, quia possimus eos. Sequi, doloribus deleniti
-      consectetur eius, numquam consequuntur accusantium perferendis culpa animi
-      voluptatibus velit exercitationem esse, iste praesentium. Architecto
-      laborum voluptatibus aspernatur id ullam quis, deleniti voluptas ducimus a
-      tempore, ex totam ad fugiat perspiciatis atque alias recusandae
-      dignissimos blanditiis harum reprehenderit ratione praesentium. Tempore
-      natus nesciunt reprehenderit ipsa voluptatibus eaque itaque dolores nisi
-      laboriosam molestias autem, eos aut nobis, repudiandae deleniti, unde ex
-      quisquam? Deleniti ad qui quidem rerum, ipsa unde maiores molestias
-      veritatis quae, neque eos velit nulla reiciendis natus dignissimos nam
-      quos debitis excepturi placeat illum. Sequi fugit voluptatum nemo possimus
-      voluptates vitae beatae unde architecto quidem asperiores soluta quaerat
-      exercitationem doloremque excepturi quos dolores iste voluptatem
-      praesentium, adipisci at aperiam? Dolorum quia, tempore porro consequatur
-      magni doloribus? Corporis tempora dolor commodi placeat iusto quisquam
-      mollitia obcaecati nobis hic quam quidem minima praesentium, id quibusdam
-      error doloribus molestias, nesciunt debitis, aut aliquid illo? Corporis
-      nam, repellendus iste perferendis quasi nostrum magni doloribus ducimus
-      reprehenderit eligendi placeat porro earum aspernatur omnis. Error
-      mollitia distinctio iste cumque similique quaerat ea dolorem laboriosam
-      ullam placeat iusto asperiores, nam dignissimos minus officia obcaecati
-      consequuntur at iure totam! Hic, voluptatum labore. Dicta ea assumenda
-      quos ex maxime consequatur expedita nulla deleniti quam perspiciatis? Eius
-      rem modi quia sit, totam, temporibus facilis veritatis obcaecati qui
-      ullam, autem neque fugiat nesciunt eligendi hic placeat tempora deleniti
-      aperiam necessitatibus! Mollitia quasi temporibus aliquid nihil,
-      excepturi, natus repudiandae iure amet eligendi, facilis reiciendis! Quae,
-      voluptatibus amet, commodi consectetur doloremque cupiditate labore quia
-      esse quas sed totam. Hic assumenda dolor earum rem deserunt provident
-      eligendi facere animi pariatur, consequuntur dicta similique sunt atque
-      aperiam omnis dolores sint accusantium ullam quasi quas nemo maiores
-      temporibus aspernatur. Expedita perspiciatis earum vel quis, consequatur
-      provident inventore debitis sunt deleniti reprehenderit numquam eos quia,
-      cumque possimus molestiae laboriosam harum officia ducimus. Aut saepe
-      tempora ullam, facere minus harum nostrum! Soluta quasi, vel quia ipsa
-      aperiam, voluptatem eligendi sapiente necessitatibus, doloremque omnis
-      temporibus? Corporis temporibus perferendis facilis maiores architecto
-      esse. Perferendis doloremque modi magnam laudantium enim delectus
-      quibusdam voluptate praesentium perspiciatis iste voluptatibus tempore
-      quos est fuga dolorum error, reiciendis autem deleniti iure provident
-      reprehenderit totam. Maiores esse voluptates eveniet rem id, rerum
-      necessitatibus consequuntur molestiae. Tempora a doloremque recusandae vel
-      necessitatibus quod ratione eaque soluta, ipsum atque. Minima, optio
-      natus, culpa quam ab, vero explicabo illo iusto fuga odio incidunt fugiat
-      unde iste expedita ut et. Ad fugiat maxime, placeat mollitia cum tempore
-      animi vel similique nihil assumenda facere perspiciatis, labore nemo
-      suscipit quis veritatis explicabo, illo minus repellat ab dolorum. Optio
-      sint odio ex aut, officiis placeat ullam eligendi, molestias nesciunt
-      deserunt a tenetur soluta? Error at deleniti nobis, nisi nihil facilis
-      voluptatibus quas quisquam. Odio, enim atque vero ut ullam distinctio
-      aliquid. Necessitatibus velit vitae error quos, dolor deserunt tempora
-      nihil voluptate ut maxime sapiente distinctio fugit adipisci minima
-      temporibus ipsum? Facilis error perspiciatis voluptatem itaque molestiae,
-      excepturi, totam ipsam incidunt impedit similique porro sunt sit nemo odit
-      est! Explicabo rerum earum tenetur, facere vel veniam vero autem vitae!
-      Molestias adipisci corrupti rem totam odit voluptatibus tempore officia
-      laudantium quasi. Alias tenetur eligendi nesciunt ipsa? Nemo aliquid
-      perspiciatis beatae dolorum animi accusantium maiores magni at. Quod dolor
-      omnis quaerat cupiditate esse dolorem voluptatum magnam nobis quibusdam.
-      Architecto provident, beatae hic voluptas quis optio ratione tempora
-      doloremque? Quod ullam distinctio nesciunt quibusdam commodi temporibus
-      fugiat aliquid ad, alias tenetur facilis porro vel possimus explicabo
-      excepturi ab. Quae, quibusdam totam eaque sit reprehenderit animi. Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Nisi harum cupiditate
-      commodi, porro nam deserunt. Maiores veritatis ex consectetur perspiciatis
-      quo nulla vel deleniti suscipit, laudantium dignissimos in reprehenderit
-      hic.
+      <Nav />
+      <SubHeaders
+        description={"INVESTMENT PRODUCTS"}
+        header={"INFRASTRUCTURE"}
+      />
+      <div className="inf-Widget-wrapper">
+        <TickerTape />
+      </div>
+
+      <div className="inf-section-wrapper">
+        <h1>Wealth Through Infrastructure</h1>
+        <div className="inf-imgAndParag">
+          <p>
+            Our goal is to emerge as the preferred infrastructure partner,
+            guiding and overseeing investments with a proven history of
+            consistently delivering appealing results to clients. This
+            achievement is driven by the dedication of our team, the depth of
+            our experience, and the robustness of our global relationships. Our
+            specialization lies in specific sectors as we actively pursue and
+            secure suitable deals. With a reliable track record spanning market
+            cycles, we adhere to a disciplined investment process, prioritizing
+            the delivery of long-term value and the preservation of capital
+            within resilient capital structures. Our sole objective is to ensure
+            predictable outcomes for our clients.
+          </p>
+          <div className="inf-iAndP-ImageWrapper">
+            <img src={infImg} alt="" />
+          </div>
+        </div>
+        <h2>What Makes Infrastructure Essential</h2>
+        <p>
+          Infrastructure assets play a crucial role by delivering essential
+          services that foster economic growth, enhance productivity, and form
+          the foundation of societal operations. These include critical
+          components such as airports, schools, energy facilities,
+          transportation networks, and communication systems.
+        </p>
+        <div className="mapWrapper">
+          <img src={mapImg} alt="" />
+        </div>
+        <p>
+          Additionally, infrastructure exhibits investor-friendly
+          characteristics, offering consistent returns across market cycles with
+          reduced volatility. It provides stable, long-term yields and serves as
+          a valuable element for portfolio diversification.
+        </p>
+        <h3>A steadfast and seasoned team with a focus on specific sectors.</h3>
+        <p>
+          Over 55 experienced infrastructure professionals located in USA ,
+          Canada and UK. Our stable team is led by founding partners.
+        </p>
+        <h3>Proactive Core Strategy</h3>
+        <p>
+          We pinpoint relative value within core assets, leveraging our profound
+          sector focus to drive substantial value.
+        </p>
+        <h3>Dynamic Asset Management</h3>
+        <p>
+          We derive value through active management, engaging stakeholders, and
+          carefully selecting internal management teams. Our approach focuses on
+          establishing leadership positions with either control or strong
+          influence to create enduring value.
+        </p>
+        <h3>Quality Pipeline Driven by Targeted Origination</h3>
+        <p>
+          With our targeted approach, we aspire to present over 210
+          opportunities annually. We actively seek opportunities where we can
+          distinguish ourselves.
+        </p>
+        <GetStartedBtn />
+      </div>
+      <Footer />
     </div>
   );
 }
