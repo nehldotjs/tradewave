25fc81b08db37991aeda43a20edcefd0ce3a5839
nehldotjs <nelsonosuya11@gmail.com>
2025-06-20 20:43:46 +0100

    new

--- src/PropAssets/GetStarted.js
+++ src/PropAssets/GetStarted.js
@@ -2,74 +2,104 @@ import React from 'react';
 import styled from 'styled-components';
 
 const GetStarted = () => {
-    return (
-        <StyledWrapper>
-            <button className="cssbuttons-io-button">
-                Get started
-                <div className="icon">
-                    <svg height={24} width={24} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M0 0h24v24H0z" fill="none" />
-                        <path d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z" fill="currentColor" />
-                    </svg>
-                </div>
-            </button>
-        </StyledWrapper>
-    );
+  return (
+    <StyledWrapper>
+      <button className="frutiger-button">
+        <div className="inner">
+          <div className="top-white" />
+          <span className="text">GET STARTED</span>
+        </div>
+      </button>
+    </StyledWrapper>
+  );
 }
 
 const StyledWrapper = styled.div`
-  .cssbuttons-io-button {
-    background: #a370f0;
-    color: white;
-    font-family: inherit;
-    padding: 0.35em;
-    padding-left: 1.2em;
-    font-size: 17px;
-    font-weight: 500;
-    border-radius: 0.9em;
-    border: none;
-    letter-spacing: 0.05em;
-    display: flex;
-    align-items: center;
-    box-shadow: inset 0 0 1.6em -0.6em #714da6;
-    overflow: hidden;
-    position: relative;
-    height: 2.8em;
-    padding-right: 3.3em;
+  .frutiger-button {
     cursor: pointer;
+    position: relative;
+    padding: 2px;
+    border-radius: 6px;
+    border: 0;
+    text-shadow: 1px 1px #000a;
+    background: linear-gradient(#006caa, #00c3ff);
+    box-shadow: 0px 4px 6px 0px #0008;
+    transition: 0.3s all;
   }
 
-  .cssbuttons-io-button .icon {
-    background: white;
-    margin-left: 1em;
+  .frutiger-button:hover {
+    box-shadow: 0px 6px 12px 0px #0009;
+  }
+
+  .frutiger-button:active {
+    box-shadow: 0px 0px 0px 0px #0000;
+  }
+
+  .inner {
+    position: relative;
+    inset: 0px;
+    padding: 1em;
+    border-radius: 4px;
+    background: radial-gradient(circle at 50% 100%, #30f8f8 10%, #30f8f800 55%),
+      linear-gradient(#00526a, #009dcd);
+    overflow: hidden;
+    transition: inherit;
+  }
+
+  .inner::before {
+    content: "";
     position: absolute;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 2.2em;
-    width: 2.2em;
-    border-radius: 0.7em;
-    box-shadow: 0.1em 0.1em 0.6em 0.2em #7b52b9;
-    right: 0.3em;
-    transition: all 0.3s;
+    inset: 0;
+    background: linear-gradient(-65deg, #0000 40%, #fff7 50%, #0000 70%);
+    background-size: 200% 100%;
+    background-repeat: no-repeat;
+    animation: thing 3s ease infinite;
   }
 
-  .cssbuttons-io-button:hover .icon {
-    width: calc(100% - 0.6em);
+  @keyframes thing {
+    0% {
+      background-position: 130%;
+      opacity: 1;
+    }
+
+    to {
+      background-position: -166%;
+      opacity: 0;
+    }
   }
 
-  .cssbuttons-io-button .icon svg {
-    width: 1.1em;
-    transition: transform 0.3s;
-    color: #7b52b9;
+  .top-white {
+    position: absolute;
+    border-radius: inherit;
+    inset: 0 -8em;
+    background: radial-gradient(
+      circle at 50% -270%,
+      #fff 45%,
+      #fff6 60%,
+      #fff0 60%
+    );
+    transition: inherit;
   }
 
-  .cssbuttons-io-button:hover .icon svg {
-    transform: translateX(0.1em);
+  .inner::after {
+    content: "";
+    position: absolute;
+    inset: 0;
+    border-radius: inherit;
+    transition: inherit;
+    box-shadow: inset 0px 2px 8px -2px #0000;
   }
 
-  .cssbuttons-io-button:active .icon {
-    transform: scale(0.95);
+  .frutiger-button:active .inner::after {
+    box-shadow: inset 0px 2px 8px -2px #000a;
+  }
+
+  .text {
+    position: relative;
+    z-index: 1;
+    color: white;
+    font-weight: 550;
+    transition: inherit;
   }`;
 
 export default GetStarted;
